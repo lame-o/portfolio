92dfedbf7cb6986dc0e4b47d6b6692afb0cd700e
liam <liamhdwight@gmail.com>
2024-12-31 13:45:28 -0800

    replaced favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Liam Dwight | Portfolio',
   description: 'Full Stack Developer Portfolio',
+  icons: {
+    icon: '/images/favicon.png',
+  },
 }
 
 export default function RootLayout({
